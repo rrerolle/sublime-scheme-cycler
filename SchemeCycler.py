be3d5ea7c3eff700e436af6d9d81405f8c4f0c74
Rémi Rérolle <rrerolle@lacie.com>
2012-02-29 11:43:27 +0100

    First version of SchemeCycler, allows easily cycling through color schemes

---
+++ SchemeCycler.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sublime
+import sublime_plugin
+import os.path
+import glob
+
+
+class CycleSchemeCommand(sublime_plugin.TextCommand):
+    def run(self, edit, backward):
+        schemes_path = os.path.join(
+            sublime.packages_path(),
+            'Color Scheme - Default',
+        )
+        schemes = [
+            os.path.basename(s)
+            for s in glob.glob(os.path.join(schemes_path, '*.tmTheme'))
+        ]
+        settings = sublime.load_settings('Preferences.sublime-settings')
+        current_scheme = os.path.basename(settings.get('color_scheme'))
+        scheme_index = schemes.index(current_scheme) + (backward and -1 or 1)
+        if scheme_index == len(schemes):
+            scheme_index = 0
+        elif scheme_index == -1:
+            scheme_index = len(schemes) - 1
+        scheme = schemes[scheme_index]
+        if not scheme:
+            return
+        settings.set(
+            'color_scheme',
+            os.path.join('Packages', 'Color Scheme - Default', scheme),
+        )
+        sublime.save_settings('Preferences.sublime-settings')
+
+        sublime.status_message(
+            u'Color Scheme: ' + os.path.splitext(scheme.decode('utf-8'))[0]
+        )
