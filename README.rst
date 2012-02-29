3445f606018aea20599ebd5f308ac4a974808590
Rémi Rérolle <rrerolle@lacie.com>
2012-02-29 12:04:16 +0100

    Small refactor, use 2 commands instead of one with an argument, add README

---
+++ README.rst
@@ -0,0 +1,31 @@
+SchemeCycler
+============
+
+The purpose of this script it to easily cycle through every available color
+schemes. This allows choosing a scheme suitable to you mood at any time.
+
+Installation
+============
+
+There is no good reason for not using Package Control:
+
+http://wbond.net/sublime_packages/package_control
+
+Just install Package Control, and select SchemeCycler in the list of available
+packages
+
+Usage
+=====
+
+You can cycle through color schemes by hitting the ``F8``, and cycle backward
+with ``Shift+F8``.
+
+You can also use the command palette, which defines the
+``"SchemeCycler: Next Color Scheme"`` and
+``"SchemeCycler: Previous Color Scheme"`` commands.
+
+Configuration
+=============
+
+To change the keyboard mappings, simply bind the ``cycle_scheme_forward`` and
+``cycle_scheme_forward`` commands.
--- SchemeCycler.py
+++ SchemeCycler.py
@@ -7,32 +7,41 @@ import os.path
 import glob
 
 
-class CycleSchemeCommand(sublime_plugin.TextCommand):
-    def run(self, edit, backward):
-        schemes_path = os.path.join(
-            sublime.packages_path(),
-            'Color Scheme - Default',
-        )
-        schemes = [
-            os.path.basename(s)
-            for s in glob.glob(os.path.join(schemes_path, '*.tmTheme'))
-        ]
-        settings = sublime.load_settings('Preferences.sublime-settings')
-        current_scheme = os.path.basename(settings.get('color_scheme'))
-        scheme_index = schemes.index(current_scheme) + (backward and -1 or 1)
-        if scheme_index == len(schemes):
-            scheme_index = 0
-        elif scheme_index == -1:
-            scheme_index = len(schemes) - 1
-        scheme = schemes[scheme_index]
-        if not scheme:
-            return
-        settings.set(
-            'color_scheme',
-            os.path.join('Packages', 'Color Scheme - Default', scheme),
-        )
-        sublime.save_settings('Preferences.sublime-settings')
+def cycle_scheme(backward=False):
+    schemes_path = os.path.join(
+        sublime.packages_path(),
+        'Color Scheme - Default',
+    )
+    schemes = [
+        os.path.basename(s)
+        for s in glob.glob(os.path.join(schemes_path, '*.tmTheme'))
+    ]
+    settings = sublime.load_settings('Preferences.sublime-settings')
+    current_scheme = os.path.basename(settings.get('color_scheme'))
+    scheme_index = schemes.index(current_scheme) + (backward and -1 or 1)
+    if scheme_index == len(schemes):
+        scheme_index = 0
+    elif scheme_index == -1:
+        scheme_index = len(schemes) - 1
+    scheme = schemes[scheme_index]
+    if not scheme:
+        return
+    settings.set(
+        'color_scheme',
+        os.path.join('Packages', 'Color Scheme - Default', scheme),
+    )
+    sublime.save_settings('Preferences.sublime-settings')
 
-        sublime.status_message(
-            u'Color Scheme: ' + os.path.splitext(scheme.decode('utf-8'))[0]
-        )
+    sublime.status_message(
+        u'Color Scheme: ' + os.path.splitext(scheme.decode('utf-8'))[0]
+    )
+
+
+class CycleSchemeForwardCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        cycle_scheme()
+
+
+class CycleSchemeBackwardCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        cycle_scheme(backward=True)
