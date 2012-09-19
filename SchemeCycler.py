a85c9d2e4ac65bb4970a565c11c87124a91dfc1e
Rémi Rérolle <rrerolle@lacie.com>
2012-09-19 16:43:49 +0200

    Improve paths handling, oddly Sublime Text only uses '/' as separator, fixes #2
    In python the os.path module is usually used to handle paths in a cross-platform
    manner. But sublime text seems to handle paths internally with a unix style.
    This forces us to use '/' separator on every platform, and prevent using the
    os.path module, which only makes things worse.
    
    It should improve things a lot when used with Windows.

--- SchemeCycler.py
+++ SchemeCycler.py
@@ -3,20 +3,37 @@
 
 import sublime
 import sublime_plugin
-import os.path
+import os
+
+
+def dirname(path):
+    return path.rsplit('/', 1)[0]
+
+
+def basename(path):
+    return path.rsplit('/', 1)[1]
+
+
+def splitext(path):
+    return path.rsplit('.', 1)
+
+
+def fix_path(path):
+    return path.replace('\\', '/')
 
 
 def cycle_scheme(backward=False):
-    package_path = sublime.packages_path()
+    package_path = fix_path(sublime.packages_path())
 
-    schemes = [os.path.join(
-            dirpath[len(os.path.dirname(package_path)) + 1:],
-            filename,
+    schemes = ['/'.join([
+                fix_path(dirpath[len(dirname(package_path)) + 1:]),
+                filename,
+            ]
         )
         for dirpath, _, filenames in os.walk(package_path)
         for filename in filenames if filename.endswith('.tmTheme')
     ]
-    schemes.sort(key=lambda x: os.path.basename(x))
+    schemes.sort(key=lambda x: basename(x))
     settings = sublime.load_settings('Preferences.sublime-settings')
     current_scheme = settings.get('color_scheme')
     scheme_index = schemes.index(current_scheme) + (backward and -1 or 1)
@@ -31,8 +48,7 @@ def cycle_scheme(backward=False):
     sublime.save_settings('Preferences.sublime-settings')
 
     sublime.status_message(
-        u'Color Scheme: ' + os.path.splitext(
-            os.path.basename(scheme).decode('utf-8'))[0]
+        u'Color Scheme: ' + splitext(basename(scheme).decode('utf-8'))[0]
     )
 
 
